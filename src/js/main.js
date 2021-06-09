a33fbb90e2d3150a39051f000c2f0237529a1dde
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-06-09 23:46:13 +0200

    Build Graphic User Interface

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// Elements of DOM
+
+const $imagesStore = document.querySelector("#images-stores");
+const $trashCan = document.querySelector("#trash-can");
+const $imgSelected = document.querySelectorAll(".imgSelected");
