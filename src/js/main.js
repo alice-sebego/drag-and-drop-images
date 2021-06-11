b02ec9044fd9dd767f5827436499edcbe9a37da4
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-06-11 23:27:20 +0200

    Refactoring user's interface

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // Elements of DOM
 
-const $imagesStore = document.querySelector("#images-stores");
+const $images = document.querySelector("#images");
 const $trashCan = document.querySelector("#trash-can");
 const $imgSelected = document.querySelectorAll(".imgSelected");
 
